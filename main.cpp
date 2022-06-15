127ca0ae259547713d4016cf4c6f90dd6414b25a
Rodrigo Machado <29443508+denioma@users.noreply.github.com>
2022-06-15 14:33:53 +0000

    Commenting

--- main.cpp
+++ main.cpp
@@ -30,21 +30,25 @@ std::vector<int> rotate(std::vector<int> vec) {
 
 bool process() {
     int odds = 0;
+    // Check that there are at most 4 colors with an odd number of pieces - four corners of the board
     for (const auto& count : colors) {
         if (count % 2 != 0) odds++;
         if (odds > 4) return false;
     }
 
+    // For every piece in the piece set
     for (int i = 0; i < n; i++) {
         std::vector<int> possibleSet;
         auto& piece = pieces[i];
         // Copy piece and sort
         auto vec = piece[0].rotation;
+        // Sorting is necessary for set_intersection to work
         std::sort(vec.begin(), vec.end());
         for (int j = 0; j < n; j++) {
             if (i == j) continue;
             // Copy comparison piece and sort
             auto other = pieces[j][0].rotation;
+            // Find common numbers
             std::sort(other.begin(), other.end());
             std::vector<int> set;
             // Find common numbers
@@ -54,6 +58,7 @@ bool process() {
             }
         }
 
+        // If a piece has no possible matches, the puzzle is impossible
         if (possibleSet.size() == 0) return false;
 
         // From possible pieces set, find those that can be placed on the right and down
@@ -93,7 +98,9 @@ void printBoard() {
     }
 }
 
+// Backtracking approach
 bool solve(int row, int col) {
+    // Scan the board from left to right, and downwards
     if (col == c) { // Wrap around
         row++;
         col = 0;
@@ -107,34 +114,36 @@ bool solve(int row, int col) {
     std::vector<index> *possible = nullptr;
 
     if (col > 0) {
-        if (row > 0) {
+        if (row > 0) { // Need to match the new piece with the one above and to the left
             auto& lidx = board[row][col - 1];
             auto& uidx = board[row - 1][col];
             auto& left = pieces[lidx[0]][lidx[1]].right;
             auto& up = pieces[uidx[0]][uidx[1]].down;
             std::set_intersection(left.begin(), left.end(), up.begin(), up.end(), std::back_inserter(tmp));
             possible = &tmp;
-        } else {
+        } else { // Need to match the new piece with the one to the left
             auto& idx = board[row][col - 1];
             possible = &pieces[idx[0]][idx[1]].right;
         }
-    } else if (row > 0) {
+    } else if (row > 0) { // Need to match the new piece with the one above
         auto& idx = board[row - 1][col];
         possible = &pieces[idx[0]][idx[1]].down;
     }
 
+    // For each possible new piece
     if (possible) {
         for (const auto& idx : *possible) {
             if (used[idx[0]]) continue; // Skip piece if already on the board
-            used[idx[0]] = true;
+            used[idx[0]] = true; // Mark piece as used
             board[row][col] = idx;
             if (solve(row, col+1)) {
                 return true;
             }
-            used[idx[0]] = false;
+            used[idx[0]] = false; // Unmark piece as used
         }
     }
 
+    // No valid partial solution found, backtrack
     col--;
     if (col < 0) {
         if (row > 0) {
@@ -159,10 +168,12 @@ int main() {
             int p1, p2, p3, p4;
             piece p;
             std::cin >> p1 >> p2 >> p3 >> p4;
+            // Count colors
             colors[p1-1]++;
             colors[p2-1]++;
             colors[p3-1]++;
             colors[p4-1]++;
+            // Store rotations
             p[0].rotation = { p1, p2, p3, p4 };
             p[1].rotation = rotate(p[0].rotation);
             p[2].rotation = rotate(p[1].rotation);
@@ -170,11 +181,11 @@ int main() {
             used[j] = (j == 0) ? true : false; // Mark only the first piece as used
             pieces.push_back(p);
         }
-        if (!process()) {
+        if (!process()) { // Preprocessing
             std::cout << "impossible puzzle!\n";
         } else {
             board[0][0] = {0, 0};
-            if (solve(0, 1)) {
+            if (solve(0, 1)) { // Solve the board
                 printBoard();
             } else {
                 std::cout << "impossible puzzle!\n";
